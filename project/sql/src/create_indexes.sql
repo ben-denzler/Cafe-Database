ff7573d13175ff03ed8ea5996b45c58b44c3437d
Benjamin Denzler <bdenz001@ucr.edu>
2022-06-06 14:11:37 -0700

    Added indexes

--- project/sql/src/create_indexes.sql
+++ project/sql/src/create_indexes.sql
@@ -1,7 +1,11 @@
-CREATE INDEX index1
-ON orders
-(timeStampRecieved);
+-- Used for displaying the 5 most recent orders
+CREATE INDEX timeStampIndex ON Orders (timeStampRecieved);
 
-CREATE INDEX index2
-ON ItemStatus
-( orderid );
+-- Used for looking up a user's orders
+CREATE INDEX loginIndex1 ON Orders (login);
+
+-- Used for logging in users
+CREATE INDEX loginIndex2 ON Users (login);
+
+-- Used for modifying/displaying orders
+CREATE INDEX orderIDIndex ON ItemStatus (orderid);
